705c31c4c7ebc0ceb40a366266d8471ef39197f0
John Gardner <77295448+GardnerJohn@users.noreply.github.com>
2021-09-04 09:23:33 -0700

    added cards and info to Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,5 +1,6 @@
 import React from "react";
 import CardItem from "./CardItem";
+import "./Cards.css";
 
 function Cards() {
   return (
@@ -8,7 +9,38 @@ function Cards() {
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
-            <CardItem />
+            <CardItem
+              src="imagesimg-9.jpg/"
+              text="Explore hidden waterfalls!"
+              label="Adventure"
+              path="/services"
+            />
+            <CardItem
+              src="imagesimg-2.jpg/"
+              text="Travel the Islands of Bali"
+              label="Beautiful Scenes"
+              path="/services"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItem
+              src="imagesimg-3.jpg/"
+              text="Set Sail in the Atlantic Ocean"
+              label="Mystery"
+              path="/services"
+            />
+            <CardItem
+              src="imagesimg-4.jpg/"
+              text="Experience Soccer in the Himilayas"
+              label="Adventure"
+              path="/products"
+            />
+            <CardItem
+              src="imagesimg-8.jpg/"
+              text="Explore the vast Sahara Desert"
+              label="Adrenaline"
+              path="/sign-up"
+            />
           </ul>
         </div>
       </div>
