79136b9ff22225d39cc4b1bce83a9149606aba8a
John Gardner <77295448+GardnerJohn@users.noreply.github.com>
2021-09-06 13:28:57 -0700

    added Products/Services/Signup pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
+import Product from "./components/pages/Products";
+import Services from "./components/pages/Services";
+import SignUp from "./components/pages/SignUp";
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
         <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/products" component={Product} />
+          <Route path="/services" component={Services} />
+          <Route path="/sign-up" component={SignUp} />
         </Switch>
       </Router>
     </>
